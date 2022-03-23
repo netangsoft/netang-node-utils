b41ef37fe70d45dc284c2489f2f907a4fe8b177c
junmiao <mj287@163.com>
2022-03-23 13:26:44 +0800

    openssl

---
+++ base64.js
@@ -0,0 +1,19 @@
+/**
+ * base64 加密
+ */
+function encrypt(value, isJson = false) {
+    return Buffer.from(isJson ? JSON.stringify(value) : value).toString('base64')
+}
+
+/**
+ * base64 解密
+ */
+function decrypt(value, isJson = false) {
+    const res = Buffer.from(value, 'base64').toString()
+    return isJson ? JSON.parse(res) : res
+}
+
+module.exports = {
+    encrypt,
+    decrypt,
+}
